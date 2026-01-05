10dfc9039c6309df6860f50b6a8ad609c33d1e80
angrypig555 <84266804+angrypig555@users.noreply.github.com>
2026-01-05 19:54:57 +0000

    begin working on main code

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-nasm -f elf32 -o main.o main.asm
-ld -m elf_i386 -o dog-ident main.o
+g++ main.cpp -o dog-ident
--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include<iostream>
 
+// sizes: 1 - small; 2 small-medium; 3 - medium; 4 medium - large; 5 large;
+
+class goldenRetriever {
+    public:
+        int coat_type = 1; // 1 = double coat
+        std::string colors[5] = {"cream", "gold", "dark gold", "dark red"};
+        int size = 4;
+};
+
 int main() {
     std::cout << "dog-ident v1, for woof" << std::endl;
     return 0;
